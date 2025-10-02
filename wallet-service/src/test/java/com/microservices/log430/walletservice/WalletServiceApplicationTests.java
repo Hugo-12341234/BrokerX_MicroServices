554a505a74914f8d22fb2cc87fe660e5c39e2a9b
Hugo Vaillant <hugo.vaillant.1@ens.etsmtl.ca>
2025-10-02 16:24:30 -0400

    Initial commit - multi-module project

---
+++ wallet-service/src/test/java/com/microservices/log430/walletservice/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.log430.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
