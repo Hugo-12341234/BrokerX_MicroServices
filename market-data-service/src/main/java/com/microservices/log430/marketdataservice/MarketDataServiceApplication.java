b0f1e10583a768ec3ec6278b5cbb89f198f5c775
Hugo Vaillant <hugo.vaillant.1@ens.etsmtl.ca>
2025-11-17 19:49:06 -0500

    Initial commit market-data-service

---
+++ market-data-service/src/main/java/com/microservices/log430/marketdataservice/MarketDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.log430.marketdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketDataServiceApplication.class, args);
+    }
+
+}
